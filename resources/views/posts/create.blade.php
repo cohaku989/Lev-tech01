4bddc7af9499a76e4de6e4d3344923ae8f00ec8c
cohaku989 <mahiro.kamienoo@gmail.com>
2022-07-14 15:57:17 +0900

    post-variety 08-4

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -19,12 +19,14 @@
             
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル"/>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             
             <div class="Body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="ここに本文"></textarea>
+                <textarea name="post[body]" placeholder="ここに本文" value="{{ old('post.body') }}"></textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             
             <input type="submit" value="store"/>
